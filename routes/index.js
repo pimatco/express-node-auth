c9a9b895562c7e8e9d3f70f188c3359835ebc8c9
pimatco <pimatco@gmail.com>
2017-10-31 13:03:23 -0200

    small refactorings

--- routes/index.js
+++ routes/index.js
@@ -15,14 +15,12 @@ router.post('/register', function(req, res, next) {
 	req.checkBody('username', 'Nome de usuário deve ser entre 4 e 15 caracteres').len(4,15);
 	req.checkBody('email', 'Seu email de usuário não pode ser em branco').isEmail();
 	req.checkBody('email', 'Seu email deve ser entre 8-100 caracteres').len(8,100);
-	req.checkBody('password', 'Senha deve incluir uma letra minúscula, uma letra maiúscula, um número e um caracter especial.').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, 'i');
-	req.checkBody('password', 'Sua senha deve ser entre 8-100 caracteres').len(8,100);
-	req.checkBody('passwordMatch', 'Sua senha deve ser entre 8-100 caracteres').len(8,100);
+	//req.checkBody('password', 'Senha deve incluir uma letra minúscula, uma letra maiúscula, um número e um caracter especial.').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, 'i');
+	req.checkBody('password', 'Sua senha deve ter entre 8-100 caracteres').len(8,100);
 	req.checkBody('passwordMatch', 'Senhas não conferem, anote a senha e tente novamente').equals(req.body.password);
 	const errors = req.validationErrors();
 	 console.log(req.body.username);
 	 console.log(req.body.email);
-	 console.log(req.body.password);
 	 console.log(req.body.password);
 
 	 if(errors){
@@ -50,10 +48,6 @@ router.post('/register', function(req, res, next) {
 	
 });
 
-/* GET home page. */
-router.get('/login', function(req, res, next) {
-	res.render('login', { title: 'Login' });
-  });
 
 module.exports = router;
 
